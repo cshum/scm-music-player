0a18c09d54b7fbd330e6310021d844f275d75e79
Adrian C Shum <me@cshum.com>
2012-10-03 02:27:50 +0800

    script link check and tidy up

--- config.js
+++ config.js
@@ -6,8 +6,16 @@ var requirejs = ({
 	paths: {
 		'jquery': 'http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min',
 		'jquery.ui': 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min',
+		'underscore': 'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.1/underscore-min',
+		'knockout':'http://cdnjs.cloudflare.com/ajax/libs/knockout/2.1.0/knockout-min',
+
+		/*
+		'jquery': 'lib/jquery/jquery-1.8.2.min',
+		'jquery.ui': 'lib/jquery/jquery-ui.min',
 		'underscore': 'lib/underscore/underscore',
 		'knockout':'lib/knockout/knockout-2.1.0',
+	   */
+
 		'text': 'lib/require/text',
 		'domready': 'lib/require/domready'
 	},
--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 		url = location.href.replace(/scmplayer\=true/g, 'scmplayer=false'),
 		host = url.substr(0,url.indexOf('/',10)),
 		src = current.getAttribute('src').replace(/script\.js/g,'scm.html')+'#'+url,
+		isOutside = !hasFrame || location.href.indexOf("scmplayer=true")>0;
 
 		addEvent = function(elm, evType, fn) {
 			if(elm.addEventListener) 
@@ -26,25 +27,24 @@
 		})(),
 		isMobile = navigator.userAgent.match(/iPad|iPhone|Android|Blackberry/i),
 
-
 		init = function(){
 			if(!document.body){ 
 				setTimeout(init,10); 
 				return;
 			}
-			if(!isMobile){
-				if(!hasFrame || location.href.indexOf("scmplayer=true")>0){
-					code();
-					outside();
-				}else
-					inside();
-			}
+			if(isOutside){
+				code();
+				outside();
+			}else
+				inside();
 		},
 
 		code = function(){
 			head.innerHTML += '<style id="scmcss" type="text/css"> html,body{overflow:hidden;} body{margin:0;padding:0;border:0;} img,a,embed,object,div,address,table,iframe,p,span,form{ display:none;border:0;margin:0;padding:0; } #scmframe{display:block; background-color:transparent; position:fixed; top:0px; left:0px; width:100%; height:100%; z-index:167;} </style>';
+			/*
 			while(head.firstChild.id!="scmcss")
 				head.removeChild(head.firstChild);
+			*/
 			
 			var scmframe = document.createElement('iframe');
 			scmframe.frameBorder = 0;
@@ -60,43 +60,70 @@
 			});
 		},
 		outside = function(){
-			//fix frame height
-			addEvent(window,'resize',function(){
-				var scmframeStyle = document.getElementById('scmframe').style;
-				scmframeStyle.height = (function(){
-					if( typeof( window.innerHeight ) == 'number' )
-						return window.innerHeight; 
-					else if( document.documentElement && document.documentElement.clientHeight ) 
-						return document.documentElement.clientHeight; 
-					else if( document.body && document.body.clientHeight ) 
-						return document.body.clientHeight; 
-				})();
-			});
+			//fix frame height in IE
+			if(isIE)
+				addEvent(window,'resize',function(){
+					var scmframeStyle = document.getElementById('scmframe').style;
+					scmframeStyle.height = (function(){
+						if( typeof( window.innerHeight ) == 'number' )
+							return window.innerHeight; 
+						else if( document.documentElement && document.documentElement.clientHeight ) 
+							return document.documentElement.clientHeight; 
+						else if( document.body && document.body.clientHeight ) 
+							return document.body.clientHeight; 
+					})();
+				});
 		},
 		inside = function(){
-			window.SCM = (function(keys){
-				var keys = keys.split(','),
-					obj = {},
-					post = function(key){
-						return function(arg){
-							var argStr = '';
-							if(typeof(arg)!='undefined')
-								argStr = (key.match(/(play|queue)/) ? 'new Song(':'(') +
-									JSON.stringify(arg)+')';
-							window.parent.postMessage('SCM.'+key+'('+argStr+')',host);
-						}
-					};
-				for(var i=0;i<keys.length;i++){
-					var key = keys[i];
-					obj[key] = post(key);
+			//fix links
+			addEvent(document.body,'click',function(e){
+				var target = e.target;
+				if(target.tagName.match(/(A|AREA)/i)){
+					if(target.href.indexOf('https://')==0 || (target.href.indexOf(location.host)==-1 &&
+					target.href.indexOf("http://")==0 ))	{
+						//external links
+						window.open(target.href,'_blank');
+						window.focus();
+						e.preventDefault();
+					}
 				}
-				return obj;
-			})('config,queue,play,pause,next,previous,volume,'+
-			'loadPlaylist,changeRepeatMode,toggleShuffle,togglePlaylist');
-
-			window.parent.postMessage('SCM.config('+config+')',host);
+			});
+			
+			//send config
+			if(config)
+				window.parent.postMessage('SCM.config('+config+')',host);
 		};
 
 	if(window.SCM) return;
+
+	//SCM interface
+	window.SCM = (function(keys){
+		var keys = keys.split(','),
+			obj = {},
+			post = function(key){
+				return function(arg){
+					if(isOutside) return;
+					var argStr = '';
+					if(typeof(arg)!='undefined')
+						argStr = (key.match(/(play|queue)/) ? 'new Song(':'(') +
+							JSON.stringify(arg)+')';
+					window.parent.postMessage('SCM.'+key+'('+argStr+')',host);
+				}
+			};
+		for(var i=0;i<keys.length;i++){
+			var key = keys[i];
+			obj[key] = post(key);
+		}
+		return obj;
+	})('config,queue,play,pause,next,previous,volume,'+
+	'loadPlaylist,changeRepeatMode,toggleShuffle,togglePlaylist');
+
 	init();
+
+	//compatibility
+	window.SCMMusicPlayer = {init: function(config){
+		if(isOutside) return;
+		window.parent.postMessage('SCM.config('+config+')',host);
+	}};
+
 })();
