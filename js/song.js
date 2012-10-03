0136c8bee88467609442117e26b6229aad48ed90
Adrian C Shum <me@cshum.com>
2012-10-03 19:28:43 +0800

    fix song isValid detection

--- js/song.js
+++ js/song.js
@@ -1,11 +1,11 @@
 define(['knockout'],function(ko){
 	return function(data){
 		var self = this;
-		this.title = ko.observable(unescape(data.title));
-		this.url = ko.observable(unescape(data.url));
+		this.title = ko.observable(unescape(data.title || ''));
+		this.url = ko.observable(unescape(data.url || ''));
 		this.on = ko.observable(false);
 		this.isValid = ko.computed(function(){
-			return self.url() && self.url()!='';
+			return (typeof self.url() === 'string') && self.url()!='';
 		});
 
 	}
