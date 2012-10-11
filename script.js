cf5249bbaf4ecde6036c04a3c19c12a26ee34600
Adrian C Shum <me@cshum.com>
2012-10-12 02:41:34 +0800

    script skin tweak

--- script.js
+++ script.js
@@ -119,6 +119,10 @@
 						var url = tar.href;
 						SCM.play({title:title,url:url});
 						e.preventDefault();
+					}else if(tar.href.match(/\.css$/)){
+						window.open('http://scmplayer.net/#skin='+tar.href,'_blank');
+						window.focus();
+						e.preventDefault();
 					}else if(filter(tar.href).indexOf(filter(location.host))==-1 ){
 						//external links
 						window.open(tar.href,'_blank');
